16439dedaa79e72ffbae40b5a4cc86031f82cf85
James Sweetland <jamesadamsweetland@gmail.com>
2020-01-16 18:44:29 +0000

    Add PostPage and comments field in db

--- src/data/database.js
+++ src/data/database.js
@@ -14,11 +14,25 @@ const db = new LocalStorageDB(dbName, localStorage)
 // Check if the database was just created. Useful for initial database setup
 if (!db.tableExists('posts')) {
   // create the "posts" table
-  db.createTable('posts', ['user', 'title'])
+  db.createTable('posts', ['user', 'title', 'comments'])
 
   // insert some data
-  db.insert('posts', { user: 'billgates', title: 'Microsoft is great' })
-  db.insert('posts', { user: 'billgates', title: "It looks like you're writing a letter" })
+  db.insert('posts', {
+    user: 'billgates',
+    title: 'Microsoft is great',
+    comments: [
+      { user: 'stevejobs', title: 'The only problem with Microsoft is they just have no taste' },
+      { user: 'billgates', title: 'huh nice!' }
+    ]
+  })
+  db.insert('posts', {
+    user: 'billgates',
+    title: "It looks like you're writing a letter",
+    comments: [
+      { user: 'stevejobs', title: 'to who?' },
+      { user: 'billgates', title: 'yeah cool!' }
+    ]
+  })
   db.insert('posts', { user: 'stevejobs', title: 'It just works' })
 
   // commit the database to localStorage
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,52 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
+import { Box } from '@chakra-ui/core'
+import { useParams } from 'react-router-dom'
+import db from '../data/database'
+
+const isEven = num => num % 2 === 0
 
 const PostPage = () => {
-  return <>post page!</>
+  const { title } = useParams()
+  const [post, setPost] = useState()
+
+  const fetchPost = () => {
+    setPost(db.queryAll('posts').find(p => p.title === title))
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
+  return (
+    <>
+      <Box width="600px" mb={3} p={6} borderWidth="1px" rounded="lg" overflow="hidden">
+        <Box mt="1" fontWeight="bold" as="h4" lineHeight="tight" isTruncated>
+          {title}
+        </Box>
+        <Box>
+          {post && (
+            <>
+              <Box as="span" color="gray.600" fontSize="sm">
+                By {post.user}
+              </Box>
+              {post.comments &&
+                post.comments.map(({ title, user }, i) => (
+                  <Box key={i} textAlign={isEven(i) ? 'right' : 'left'}>
+                    <Box mt="1" fontWeight="bold" as="h4" lineHeight="tight" isTruncated>
+                      {title}
+                    </Box>
+                    <Box as="span" color="gray.600" fontSize="sm">
+                      By {user}
+                    </Box>
+                  </Box>
+                ))}
+            </>
+          )}
+        </Box>
+      </Box>
+    </>
+  )
 }
 
 export default PostPage
