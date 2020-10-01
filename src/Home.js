fc5b9d05c549e72fcd2fb04ebc66a88afb6cd57c
Sean <scjwalsh@gmail.com>
2020-10-01 08:40:50 +0300

    Added welcome message to home screen with button to create profile

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Card from './ui/Card'
-import DataAccessDemo from './features/DemoDataAccess'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Text, Button } from "@chakra-ui/core";
 
-const Home = ({username}) => {
-    return <DataAccessDemo username={username} />
-    
-
+export default function Home({ username }) {
+  return (
+    <section>
+      <Text fontSize="4xl">Welcome {username}</Text>
+      <Link to="/create-profile">
+        <Button>Create your profile</Button>
+      </Link>
+    </section>
+  );
 }
-
-export default Home
-
-
-
-              
